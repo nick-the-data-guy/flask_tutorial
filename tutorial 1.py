0227e37431aa7d75323654f415663a0883a725c2
Nick White <lostipod12@gmail.com>
2023-09-13 23:25:34 -0700

    added nick_instructions.txt

--- tutorial 1.py
+++ tutorial 1.py
@@ -1,12 +1,26 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 from waitress import serve 
 
 app = Flask(__name__)
 
-@app.route("/")
 
+
+@app.route("/")
 def home():
-    return "This is Nick's flask page"
+    return "This is Nick's flask page <h1> HELLO</h1>"
+
+
+@app.route("/<name>")
+def user(name):
+    return f"Hello {name}!"
+
+
+@app.route("/admin")
+def admin():
+    return redirect(url_for("home"))
+
+
+
 
 if __name__=="__main__":
     serve(app, host="0.0.0.0", port=8000)
