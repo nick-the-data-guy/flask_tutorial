ee7d2e96ddfff61e9885c6f63e1fa143bf996f88
Nick White <lostipod12@gmail.com>
2023-09-14 17:42:58 -0700

    end of tutorial 2

--- tutorial 2.py
+++ tutorial 2.py
@@ -1,20 +1,11 @@
 from flask import Flask, redirect, url_for, render_template
 from waitress import serve
 
-
-
 app = Flask(__name__)
 
-
-
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-
-
-
-
+@app.route("/<name>")
+def home(name):
+    return render_template("index.html", content = ["tim", "joe", "bill"])
 
 
 if __name__=="__main__":
